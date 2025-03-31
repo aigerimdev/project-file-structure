f802a3e15e7b0437fd147ed3431ecd2cd86cb13d
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-03-31 15:11:38 -0700

    implements folder structure

--- app/multiplication.py
+++ app/multiplication.py
@@ -1,4 +1,4 @@
-import addition
+import app.addition as addition
 
 def perform_operation(multiplier, multiplicand):
     result = 0
--- tests/test_subtraction.py
+++ tests/test_subtraction.py
@@ -1,4 +1,4 @@
-import subtraction
+import app.subtraction as subtraction
 
 def test_subtraction():
     # Assert
